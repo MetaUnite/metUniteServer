cd211e7fd856c8d47b764199a5f6448404e7b7e6
MetaUnite <123376653+MetaUnite@users.noreply.github.com>
2023-03-07 17:39:31 +0530

    Admin Model Created

--- model/adminModel.js
+++ model/adminModel.js
@@ -1,1 +1,53 @@
 // Creating Admin model
+// Creating Admin model
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    mobile_number: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    otp: {
+      //when admin forgot his password, he will be asked to verify his email address using otp
+      type: String,
+    },
+    otpTime: {
+      type: Date,
+      default: Date.now,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("admin", adminSchema);
